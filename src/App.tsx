784c81a30d44e0e9595eb10fdc66c6c761e62dc4
Yeonhan Jung <yt4307@gmail.com>
2023-09-16 15:03:40 +0900

    feat: router 추가 및 kakaomap 경로 변경 - / -> /kakaomap

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,17 @@
-import styled from "styled-components";
-import KaKaoMap from "components/KakaoMap";
+import KakaoMap from "components/KakaoMap";
+import { Route, Routes } from "react-router-dom";
+
+import HomePage from "pages/HomePage";
 
 const App = () => {
+  const devURL = process.env.PUBLIC_URL ? "/proxy/3000" : "";
+
   return (
-    <Container>
-      <KaKaoMap />
-    </Container>
+    <Routes>
+      <Route path={`${devURL}/`} element={<HomePage />} />
+      <Route path={`${devURL}/kakaomap`} element={<KakaoMap />} />
+    </Routes>
   );
 };
-
-const Container = styled.div`
-  display: flex;
-
-  width: 100vw;
-  height: 100vh;
-
-  justify-content: center;
-  align-items: center;
-`;
 
 export default App;
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+const HomePage = () => {
+  return (
+    <Container>
+      <h1>TuTi 웹 페이지는 아직 개발 중입니다!</h1>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export default HomePage;
